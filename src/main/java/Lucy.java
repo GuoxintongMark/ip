8bf9fc4fe138316bbd63d2be8afcad2b37288190
Mark <e1481831@u.nus.edu>
2026-01-25 14:16:40 +0800

    Update Lucy.java to echo user input with smiley

--- src/main/java/Lucy.java
+++ src/main/java/Lucy.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Lucy {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Initial greeting
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Lucy");
-        System.out.println(" What can I do for you?");
+        System.out.println("-> Hello! I'm Lucy. (′゜ω。‵)");
+        System.out.println("-> What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            // Read user input
+            String command = scanner.nextLine().trim();
+
+            // bye
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("-> Bye. Hope to see you again soon! (*´∀`)~♥");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            // Echo command back
+            System.out.println("____________________________________________________________");
+            System.out.println("-> " + command);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
